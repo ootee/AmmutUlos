bcc57d7b49ae1522d30eae16651b61346e81605b
Olli Tanttu <olli.tanttu@helsinki.fi>
2017-08-01 20:26:59 +0300

    Sivusuunnitelmat tehty.

--- app/controllers/hello_world_controller.php
+++ app/controllers/hello_world_controller.php
@@ -4,11 +4,35 @@
 
     public static function index(){
       // make-metodi renderöi app/views-kansiossa sijaitsevia tiedostoja
-   	  View::make('home.html');
+      View::make('suunnitelmat/etusivu.html');
     }
+
+    public static function kilpailulista(){
+      View::make('suunnitelmat/kilpailulista.html');
+    }
+
+    public static function kilpailulista_edit(){
+      View::make('suunnitelmat/kilpailulista_edit.html');
+    }
+
+    public static function kilpailu(){
+      View::make('suunnitelmat/kilpailu.html');
+    }
+
+    public static function kilpailu_edit(){
+      View::make('suunnitelmat/kilpailu_edit.html');
+    }
+
+    public static function rasti(){
+      View::make('suunnitelmat/rasti.html');
+    }
+
+    public static function rasti_edit(){
+      View::make('suunnitelmat/rasti_edit.html');
+    }    
 
     public static function sandbox(){
       // Testaa koodiasi täällä
-      echo 'Hello World!';
+      View::make('helloworld.html');
     }
   }
--- config/routes.php
+++ config/routes.php
@@ -4,6 +4,32 @@
     HelloWorldController::index();
   });
 
+  $routes->get('/kilpailulista', function() {
+  	HelloWorldController::kilpailulista();
+  });
+
+  $routes->get('/kilpailulista_edit', function() {
+  	HelloWorldController::kilpailulista_edit();
+  });
+
+  $routes->get('/kilpailu', function() {
+  	HelloWorldController::kilpailu();
+  });
+
+  $routes->get('/kilpailu_edit', function() {
+  	HelloWorldController::kilpailu_edit();
+  });
+
+  $routes->get('/rasti', function() {
+  	HelloWorldController::rasti();
+  });
+
+  $routes->get('/rasti_edit', function() {
+  	HelloWorldController::rasti_edit();
+  });
+
   $routes->get('/hiekkalaatikko', function() {
     HelloWorldController::sandbox();
   });
+
+
