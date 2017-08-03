0a4d6e457f13f8aa55e1082b915153ecf8a66aee
Olli Tanttu <olli.tanttu@helsinki.fi>
2017-08-03 20:47:06 +0300

    Luonti- ja poistolauseet ja testidata
    se "git push" to publish your local commits)

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,6 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Kilpailija (etunimi, sukunimi) VALUES ('Pekka', 'Puupää');
+INSERT INTO Kilpailu (pvm, paikka) VALUES ('2017-08-01', 'Helsinki');
+INSERT INTO Rasti (rastinro, kuvaus, taululkm, kilpailu) VALUES ('1', 'kolme taulua', '3', '1');
+INSERT INTO Tulos (aika, pisteet, rasti, kilpailija) VALUES ('2.56', '24', '1', '1');
+INSERT INTO Kilpailusuoritus (kilpailija, kilpailu) VALUES ('1', '1');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,33 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Kilpailija (
+	kilpailija_id SERIAL PRIMARY KEY,
+	etunimi TEXT NOT NULL,
+	sukunimi TEXT
+);
+
+CREATE TABLE Kilpailu (
+	kilpailu_id SERIAL PRIMARY KEY,
+	pvm DATE NOT NULL,
+	paikka TEXT NOT NULL
+);
+
+CREATE TABLE Rasti (
+	rasti_id SERIAL PRIMARY KEY,
+	rastinro INTEGER NOT NULL,
+	kuvaus VARCHAR(100),
+	taululkm INTEGER NOT NULL,
+	kilpailu INTEGER REFERENCES Kilpailu(kilpailu_id)
+);
+
+CREATE TABLE Tulos (
+	tulos_id SERIAL PRIMARY KEY,
+	aika DECIMAL NOT NULL,
+	pisteet INTEGER NOT NULL,
+	rasti INTEGER REFERENCES Rasti(rasti_id),
+	kilpailija INTEGER REFERENCES Kilpailija(kilpailija_id)
+);
+
+CREATE TABLE Kilpailusuoritus (
+	kilpailija INTEGER REFERENCES Kilpailija(kilpailija_id),
+	kilpailu INTEGER REFERENCES Kilpailu(kilpailu_id)
+);
